9c70f6a2ab06703674b36941d2663a4534144d94
smike <michal.szapowalow@netinteractive.pl>
2015-06-12 16:04:48 +0200

    change params

--- src/Netinteractive/Combiner/Combiner.php
+++ src/Netinteractive/Combiner/Combiner.php
@@ -4,65 +4,76 @@ use Netinteractive\Utils\Utils;
 use Netinteractive\Combiner\Interfaces\CombinerInterface;
 
 
-class Combiner implements  CombinerInterface{
+class Combiner implements CombinerInterface
+{
 
-	static function incFile($fromPaths, $toPath, $extension=null){
+    /**
+     * Skleja i zapisju pliki do jedengo plika i zwraca url do tego pliku
+     * @param array $fromPaths - lista plikow lub folderow
+     * @param string $toPath - plik do ktorego trzeba zapisac
+     * @param null $extension - rozrzezenie pliku
+     * @return string - url zapisanego pliku
+     */
+    static function incFile($fromPaths, $toPath, $skin = null, $extension = null)
+    {
 
-		if(!is_array($fromPaths)){
-			$fromPaths=array($fromPaths);
-		}
-		$toPath=public_path($toPath);
-		$pathInfo=pathinfo($toPath);
-		$fName=$pathInfo['basename'];
-		$fName=explode('.',$fName);
-		if(!$extension){
-			$extension=array_pop($fName);
-		}
-		else{
-			array_pop($fName);
-		}
+        if (!is_array($fromPaths)) {
+            $fromPaths = array($fromPaths);
+        }
 
-		$fName=implode('.',$fName).'.'.\Gettext::getLocale().'.'.$extension;
-		$toPath=$pathInfo['dirname'].DIRECTORY_SEPARATOR.$fName;
+        $toPath = public_path($toPath);
+        $pathInfo = pathinfo($toPath);
+        $fName = $pathInfo['basename'];
+        $fName = explode('.', $fName);
 
+        if (!$extension) {
+            $extension = array_pop($fName);
+        } else {
+            array_pop($fName);
+        }
 
-		$paths=array();
-
-		if(\Config::get('app.debug') || !file_exists($toPath)){
-			foreach($fromPaths as $path){
-
-				$path=public_path($path);
-
-				if(is_dir($path)){
-					$paths=array_merge($paths,Utils::scanDir($path,'.'.$extension,true));
-				}
-				else{
-					$paths[]=$path;
-				}
-
-			}
-			$paths=array_unique($paths);
-			$text='';
-			foreach($paths as $path){
-				$text.=file_get_contents($path)."\n";
-			}
-
-			$text=preg_replace_callback("/php\(\/\*(.*)\*\/\)/",function($matches){
-				return eval("return json_encode(".$matches[1].');');
-			},$text);
+        $fName = implode('.', $fName) . '.' . \App::getLocale() . '.' . $extension;
+        $toPath = $pathInfo['dirname'] . DIRECTORY_SEPARATOR . $fName;
 
 
-			file_put_contents($toPath,$text);
-		}
+        $paths = array();
 
-		$publicPath = public_path();
+        if (\Config::get('app.debug') || !file_exists($toPath)) {
+            foreach ($fromPaths as $path) {
 
-		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
-			if ( $publicPath[count($publicPath)-1] != '\\' ){
-				$publicPath .= '\\';
-			}
-		}
+                $path = base_path($path);
 
-		return asset(str_replace($publicPath,'',$toPath));
-	}
+                if (is_dir($path)) {
+                    $scannedPaths = Utils::scanDir($path, '.' . $extension, true);
+
+                    $paths = array_merge($paths, $scannedPaths);
+                } else {
+                    $paths[] = $path;
+                }
+
+            }
+            $paths = array_unique($paths);
+            $text = '';
+            foreach ($paths as $path) {
+                $text .= file_get_contents($path) . "\n";
+            }
+
+            $text = preg_replace_callback("/php\(\/\*(.*)\*\/\)/", function ($matches) {
+                return eval("return json_encode(" . $matches[1] . ');');
+            }, $text);
+
+
+            file_put_contents($toPath, $text);
+        }
+
+        $publicPath = public_path();
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            if ($publicPath[count($publicPath) - 1] != '\\') {
+                $publicPath .= '\\';
+            }
+        }
+
+        return asset(str_replace($publicPath, '', $toPath));
+    }
 }
