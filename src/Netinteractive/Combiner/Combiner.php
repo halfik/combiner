c2389e6a5c0a42deef379cf7ff5c77668647617d
smike <michal.szapowalow@netinteractive.pl>
2015-06-22 13:14:03 +0200

    Refactoring

--- src/Netinteractive/Combiner/Combiner.php
+++ src/Netinteractive/Combiner/Combiner.php
@@ -7,7 +7,7 @@ use Netinteractive\Combiner\Interfaces\CombinerInterface;
 class Combiner implements  CombinerInterface{
 
     /**
-     * Skleja i zapisju pliki do jedengo plika i zwraca url do tego pliku
+     * Skleja i zapisjue pliki do jedengo plika i zwraca url do tego pliku
      * @param array $fromPaths - lista plikow lub folderow
      * @param string $toPath - plik do ktorego trzeba zapisac
      * @param null $extension - rozrzezenie pliku
@@ -15,52 +15,7 @@ class Combiner implements  CombinerInterface{
      */
 	static function incFile($fromPaths, $toPath, $extension=null){
 
-		if(!is_array($fromPaths)){
-			$fromPaths=array($fromPaths);
-		}
-		$toPath=public_path($toPath);
-		$pathInfo=pathinfo($toPath);
-		$fName=$pathInfo['basename'];
-		$fName=explode('.',$fName);
-		if(!$extension){
-			$extension=array_pop($fName);
-		}
-		else{
-			array_pop($fName);
-		}
-
-		$fName=implode('.',$fName).'.'.\App::getLocale().'.'.$extension;
-		$toPath=$pathInfo['dirname'].DIRECTORY_SEPARATOR.$fName;
-
-
-		$paths=array();
-
-		if(\Config::get('app.debug') || !file_exists($toPath)){
-			foreach($fromPaths as $path){
-
-				$path=base_path($path);
-
-				if(is_dir($path)){
-					$paths=array_merge($paths,Utils::scanDir($path,'.'.$extension,true));
-				}
-				else{
-					$paths[]=$path;
-				}
-
-			}
-			$paths=array_unique($paths);
-			$text='';
-			foreach($paths as $path){
-				$text.=file_get_contents($path)."\n";
-			}
-
-			$text=preg_replace_callback("/php\(\/\*(.*)\*\/\)/",function($matches){
-				return eval("return json_encode(".$matches[1].');');
-			},$text);
-
-
-			file_put_contents($toPath,$text);
-		}
+		static::glueFiles($fromPaths, $toPath, $extension);
 
 		$publicPath = public_path();
 
@@ -72,4 +27,56 @@ class Combiner implements  CombinerInterface{
 
 		return asset(str_replace($publicPath,'',$toPath));
 	}
+
+    /**
+     * Lączy pliki w jeden
+     * @param array|string $fromPaths
+     * @param string $toPath
+     * @param string $extension
+     */
+    static function glueFiles($fromPaths, $toPath, $extension=null){
+        if(!is_array($fromPaths)){
+            $fromPaths=array($fromPaths);
+        }
+        $pathInfo=pathinfo($toPath);
+        $fName=$pathInfo['basename'];
+        $fName=explode('.',$fName);
+        if(!$extension){
+            $extension=array_pop($fName);
+        }
+        else{
+            array_pop($fName);
+        }
+
+        $paths=array();
+
+        if(\Config::get('app.debug') || !file_exists($toPath)){
+            foreach($fromPaths as $path){
+
+                if(is_dir($path)){
+                    $paths=array_merge($paths,Utils::scanDir($path,'.'.$extension,true));
+                }
+                else{
+                    $paths[]=$path;
+                }
+
+            }
+            $paths=array_unique($paths);
+            $text='';
+            foreach($paths as $path){
+                $text.=file_get_contents($path)."\n";
+            }
+
+            $text=preg_replace_callback("/php\(\/\*(.*)\*\/\)/",function($matches){
+                return eval("return json_encode(".$matches[1].');');
+            },$text);
+
+
+            file_put_contents($toPath,$text);
+        }
+    }
+
+    static function includeSkin($paths){
+
+    }
 }
